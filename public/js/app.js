d515a3f383ab891ef2c659ea94e1900dd7ea49f6
James Wilson <jameswilson@Taras-iMac.local>
2021-08-22 21:20:23 +0100

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+console.log('Client side JavaScript file is loaded')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+const getForecast = location => {
+    fetch(`http://localhost:3000/weather?address=${encodeURIComponent(location)}`).then(response => {
+    response.json().then((data) => {
+        if (data.error) { 
+            messageOne.textContent = data.error
+        } else {
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        }    
+    })
+})
+}
+
+weatherForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent='Loading...'
+    messageTwo.textContent=''
+
+    getForecast(location)
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=42bfc980f851a134576ac49fa03cd28e&query=${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}`
+
+    request( {url: url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined)
+        }
+        else if (body.error) {
+            callback('Unable to find location', undefined)
+        }
+        else {
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees. It feels like ${body.current.temperature} degrees.`)
+        }
+    })
+}
+
+module.exports = forecast
