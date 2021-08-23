1de22df4841e78314652e11e6c62edc249d8e00e
James Wilson <jawi13@outlook.com>
2021-08-23 11:19:32 +0100

    package update/formatting

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,8 @@ const forecast = (longitude, latitude, callback) => {
             callback('Unable to find location', undefined)
         }
         else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees. It feels like ${body.current.feelslike} degrees. The humidity is ${body.current.humidity}%.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees. It feels like ${body.current.feelslike} degrees.
+             The humidity is ${body.current.humidity}%.`)
         }
     })
 }
